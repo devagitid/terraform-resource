3ec4065dbca5628ba24388984052988d734a8958
root <root@VM1.w0mdn5ml2hkejgxrrb1znadzte.cx.internal.cloudapp.net>
2022-06-26 12:31:19 +0000

    multi vm deployment code

--- main.tf
+++ main.tf
@@ -1,133 +1,123 @@
-resource "random_pet" "rg-name" {
-  prefix    = var.resource_group_name_prefix
+# Create a resource group
+resource “azurerm_resource_group” “example_rg” {
+name = “${var.resource_prefix}-RG”
+location = var.node_location
 }
 
-resource "azurerm_resource_group" "rg" {
-  name      = random_pet.rg-name.id
-  location  = var.resource_group_location
+# Create a virtual network within the resource group
+resource “azurerm_virtual_network” “example_vnet” {
+name = “${var.resource_prefix}-vnet”
+resource_group_name = azurerm_resource_group.example_rg.name
+location = var.node_location
+address_space = var.node_address_space
 }
 
-# Create virtual network
-resource "azurerm_virtual_network" "myterraformnetwork" {
-  name                = "myVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# Create a subnets within the virtual network
+resource “azurerm_subnet” “example_subnet” {
+name = “${var.resource_prefix}-subnet”
+resource_group_name = azurerm_resource_group.example_rg.name
+virtual_network_name = azurerm_virtual_network.example_vnet.name
+address_prefix = var.node_address_prefix
 }
 
-# Create subnet
-resource "azurerm_subnet" "myterraformsubnet" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
-  address_prefixes     = ["10.0.1.0/24"]
+# Create Linux Public IP
+resource “azurerm_public_ip” “example_public_ip” {
+count = var.node_count
+name = “${var.resource_prefix}-${format(“%02d”, count.index)}-PublicIP”
+#name = “${var.resource_prefix}-PublicIP”
+location = azurerm_resource_group.example_rg.location
+resource_group_name = azurerm_resource_group.example_rg.name
+allocation_method = var.Environment == “Test” ? “Static” : “Dynamic”
+
+tags = {
+environment = “Test”
+}
 }
 
-# Create public IPs
-resource "azurerm_public_ip" "myterraformpublicip" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+## Create Network Interface
+resource “azurerm_network_interface” “example_nic” {
+count = var.node_count
+#name = “${var.resource_prefix}-NIC”
+name = “${var.resource_prefix}-${format(“%02d”, count.index)}-NIC”
+location = azurerm_resource_group.example_rg.location
+resource_group_name = azurerm_resource_group.example_rg.name
+#
+
+ip_configuration {
+name = “internal”
+subnet_id = azurerm_subnet.example_subnet.id
+private_ip_address_allocation = “Dynamic”
+public_ip_address_id = element(azurerm_public_ip.example_public_ip.*.id, count.index)
+#public_ip_address_id = azurerm_public_ip.example_public_ip.id
+#public_ip_address_id = azurerm_public_ip.example_public_ip.id
+}
 }
 
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "myterraformnsg" {
-  name                = "myNetworkSecurityGroup"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# Creating resource NSG
+resource “azurerm_network_security_group” “example_nsg” {
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+name = “${var.resource_prefix}-NSG”
+location = azurerm_resource_group.example_rg.location
+resource_group_name = azurerm_resource_group.example_rg.name
+
+# Security rule can also be defined with resource azurerm_network_security_rule, here just defining it inline.
+security_rule {
+name = “Inbound”
+priority = 100
+direction = “Inbound”
+access = “Allow”
+protocol = “Tcp”
+source_port_range = “*”
+destination_port_range = “*”
+source_address_prefix = “*”
+destination_address_prefix = “*”
+
+}
+tags = {
+environment = “Test”
+}
 }
 
-# Create network interface
-resource "azurerm_network_interface" "myterraformnic" {
-  name                = "myNIC"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# Subnet and NSG association
+resource “azurerm_subnet_network_security_group_association” “example_subnet_nsg_association” {
+subnet_id = azurerm_subnet.example_subnet.id
+network_security_group_id = azurerm_network_security_group.example_nsg.id
 
-  ip_configuration {
-    name                          = "myNicConfiguration"
-    subnet_id                     = azurerm_subnet.myterraformsubnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
-  }
 }
 
-# Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.myterraformnic.id
-  network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+# Virtual Machine Creation — Linux
+resource “azurerm_virtual_machine” “example_linux_vm” {
+count = var.node_count
+name = “${var.resource_prefix}-${format(“%02d”, count.index)}”
+#name = “${var.resource_prefix}-VM”
+location = azurerm_resource_group.example_rg.location
+resource_group_name = azurerm_resource_group.example_rg.name
+network_interface_ids = [element(azurerm_network_interface.example_nic.*.id, count.index)]
+vm_size = “Standard_A1_v2”
+delete_os_disk_on_termination = true
+
+storage_image_reference {
+publisher = “OpenLogic”
+offer = “CentOS”
+sku = “7.5”
+version = “latest”
+}
+storage_os_disk {
+name = “myosdisk-${count.index}”
+caching = “ReadWrite”
+create_option = “FromImage”
+managed_disk_type = “Standard_LRS”
+}
+os_profile {
+computer_name = “linuxhost”
+admin_username = “terminator”
+admin_password = “Password@1234”
+}
+os_profile_linux_config {
+disable_password_authentication = false
 }
 
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
-
-  byte_length = 8
+tags = {
+environment = “Test”
 }
-
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "mystorageaccount" {
-  name                     = "diag${random_id.randomId.hex}"
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
 }
-
-# Create (and display) an SSH key
-resource "tls_private_key" "example_ssh" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# Create virtual machine
-resource "azurerm_linux_virtual_machine" "myterraformvm" {
-  count			= 2
-  name                  = "myVM$(count.index)"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-  size                  = "Standard_DS1_v2"
-
-  os_disk {
-    name                 = "myOsDisk"
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18_04-lts-gen2"
-    version   = "latest"
-  }
-
-  computer_name                   = "myvm"
-  admin_username                  = "azureuser"
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = tls_private_key.example_ssh.public_key_openssh
-  }
-
-  boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-  }
-}
-
