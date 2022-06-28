52f59635b84ba73ab60c0408a60d88f3d2c1d22d
devagitid <104121621+devagitid@users.noreply.github.com>
2022-06-28 17:29:57 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create a resource group
 resource "azurerm_resource_group" "example_rg" {
-name = ""var.resource_prefix"-RG"
+name = "${var.resource_prefix}-RG"
 location = "var.node_location"
 }
 
